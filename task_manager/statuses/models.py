459197ecc6f39cc9ce794c0c27055ab9d20ea3db
Igor_Aukhadeev <rumbks@gmail.com>
2022-07-14 11:57:31 +0500

    Add __str__ method for correct representation in forms

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -3,5 +3,8 @@ from django.utils import timezone
 
 
 class Status(models.Model):
+    def __str__(self):
+        return self.name
+
     name = models.CharField(unique=True, max_length=150)
     date_created = models.DateTimeField(default=timezone.now)
