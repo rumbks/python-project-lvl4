b8c20350f659f87a8fb1c2ee326ab5cd629e9608
Igor_Aukhadeev <rumbks@gmail.com>
2022-07-16 19:27:27 +0500

    Change user stringification

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -5,18 +5,27 @@ from django.contrib.auth import get_user_model
 from task_manager.labels.models import Label
 from task_manager.statuses.models import Status
 
+User = get_user_model()
+
+
+def stringify_user(self):
+    return f"{self.first_name} {self.last_name}"
+
+
+User.__str__ = stringify_user
+
 
 class Task(models.Model):
     name = models.CharField(unique=True, max_length=150)
     description = models.CharField(max_length=1000, blank=True)
     status = models.ForeignKey(Status, on_delete=models.PROTECT)
     author = models.ForeignKey(
-        get_user_model(),
+        User,
         on_delete=models.PROTECT,
         related_name="tasks_created_set",
     )
     assignee = models.ForeignKey(
-        get_user_model(),
+        User,
         on_delete=models.PROTECT,
         blank=True,
         null=True,
